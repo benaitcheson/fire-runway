d67a7699f76458d4bd3619fa727bc30d0823d6bd
Ben Aitcheson <benaitcheson@hotmail.com>
2024-08-28 19:10:07 +1000

    ADD: sessions and login buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root "home#welcome"
+  get 'dashboard/index'
 
   # user sign up
   get "sign_up", to: "users#new", as: :sign_up
   post "sign_up", to: "users#create"
   get 'confirmations/confirm_email/:confirmation_token', to: 'confirmations#confirm_email', as: :email_confirmation
+
+  # user sign in
+  get "login", to: "sessions#new", as: :login
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy", as: :logout
 end
