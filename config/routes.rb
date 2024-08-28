2ad273b7b1e2edd3b04fcfc0c6ad2e689052c146
Ben Aitcheson <benaitcheson@hotmail.com>
2024-08-28 18:55:08 +1000

    email confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#welcome"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # user sign up
+  get "sign_up", to: "users#new", as: :sign_up
+  post "sign_up", to: "users#create"
+  get 'confirmations/confirm_email/:confirmation_token', to: 'confirmations#confirm_email', as: :email_confirmation
 end
---
+++ db/migrate/20240828082445_rename_encrypted_password_to_password_digest_in_users.rb
@@ -0,0 +1,5 @@
+class RenameEncryptedPasswordToPasswordDigestInUsers < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :encrypted_password, :password_digest
+  end
+end
