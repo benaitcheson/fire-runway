0ad8814f86bbc878f31a486fd89369bea85aad67
Ben Aitcheson <benaitcheson@hotmail.com>
2024-08-29 07:55:46 +1000

    ADD: asset table and money gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_28_083124) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_213445) do
+  create_table "user_assets", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.integer "purchase_price_cents", null: false
+    t.string "purchase_price_currency", default: "AUD", null: false
+    t.date "purchase_date", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_assets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -20,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_28_083124) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "user_assets", "users"
 end
